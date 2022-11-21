a5ad39614dc1a336e9f716763f881e88bd827236
Gastone Canessa <34108337+GastoneCanessa@users.noreply.github.com>
2022-11-21 18:55:51 +0100

    Delete env directory

--- env/Scripts/mididumphw.py
+++
@@ -1,9 +0,0 @@
-#!C:\Users\Utente\Desktop\py_midi\env\Scripts\python.exe
-"""
-Print a description of the available devices.
-"""
-import midi.sequencer as sequencer
-
-s = sequencer.SequencerHardware()
-
-print(s)
